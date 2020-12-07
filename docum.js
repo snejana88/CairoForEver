c745c2e2a435ef8d6383148dd578e68958b286ab
unknown <stefanova1319@gmail.com>
2020-12-07 13:51:33 +0100

    versJana

---
+++ docum.js
@@ -0,0 +1,18 @@
+const buttonSuma = document.getElementById("summar");
+const buttonRest = document.getElementById("restar");
+const textContador =document.getElementById("cuenta");
+let contador = 0
+
+
+buttonSuma.addEventListener("click", ()=>{
+    contador=contador+1;
+textContador.textContent= contador;
+
+})
+
+buttonRest.addEventListener("click", ()=>{
+   contador--;
+   textContador.textContent=contador;
+   
+})
+
